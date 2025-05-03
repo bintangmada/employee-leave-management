5aebb04abb63f09dc296e17275b32eb823a68107
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-05-04 02:14:09 +0700

    update

--- src/main/java/employee_leave_management/controller/LeaveRequestController.java
+++ src/main/java/employee_leave_management/controller/LeaveRequestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/leave-requests")
+        @RequestMapping("/leave-requests")
 public class LeaveRequestController {
 
     @Autowired
